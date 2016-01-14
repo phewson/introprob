fe26c336676bc7c03013895c131bbd62c5b20cf0
phewson <paul.hewson@plymouth.ac.uk>
2016-01-14 17:17:26 +0000

    ADDED PERMUTATIONS CHAPTER (IN VERY DRAFT FORM)

--- Problems/Master.R
+++ Problems/Master.R
@@ -1,5 +1,6 @@
 install.packages("exams")
 
 require(exams)
-chapter1 <- c("Set1//SampleSpaces.Rnw", "Set1/BasicSets.Rnw", "Set1//CoinTossing.Rnw", "Set1//AdditionRuleProof.Rnw", "Set1//AdditionRule.Rnw")
-exams2pdf(chapter1, n = 1, tdir = "/Users/phewson", dir = "/Users/phewson")
+chapter1 <- c("Set1/SampleSpaces.Rnw", "Set1/BasicSets.Rnw", "Set1/CoinTossing.Rnw", 
+              "Set1/AdditionRuleProof.Rnw", "Set1/AdditionRule.Rnw")
+exams2pdf(chapter1, n = 1, tdir = "dummy1", dir = "dummy2")
