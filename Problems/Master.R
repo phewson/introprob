6852bf1323cb1392eae8b71aa152c86cd3adec02
phewson <paul.hewson@plymouth.ac.uk>
2016-03-08 17:45:11 +0000

    Desnagging some small errors

--- Problems/Master.R
+++ Problems/Master.R
@@ -9,20 +9,27 @@ require(exams)
 chapter2 <- c("Set1/SampleSpaces.Rnw", "Set1/CoinTossing.Rnw", 
             "Set1/Venns1.Rnw", "Set1/Venns2.Rnw")#, "Set1/DeMorgan.Rnw")
 exams2pdf(chapter2, n = 1, tdir = "dummy2", verbose=TRUE)
-#exams2moodle(chapter2, n=5)
+exams2moodle(chapter2, n=5)
 
+chapter2m <- c("Set1/CoinTossing.Rnw", 
+"Set1/Venns1.Rnw", "Set1/Venns2.Rnw")
+exams2moodle(chapter2m, n=5)
 
 chapter3 <- c("Set1/ValidProb.Rnw", "Naive/family.Rnw")
-exams2pdf(chapter3, n=1, tdir = "dummy3", template="plain")
+exams2pdf(chapter3, n=1, tdir = "dummy3")
+exams2moodle(chapter3, n=5)
+
 
 chapter4 <- c("Comb1/CommitteeGender.Rnw", "Comb1/Committee.Rnw", "Comb1/Lottery.Rnw",
               "Comb1/AngelinaJolie.Rnw", "Comb1/AngelinaJolie2.Rnw", "Comb1/AngelinaJolie3.Rnw", "Comb1/AngelinaJolie4.Rnw")
 exams2pdf(chapter4, n = 1, tdir = "dummy4")
+exams2moodle(chapter4, n=5)
+
 
 chapter5 <- c("Set1/AdditionRuleProof.Rnw", "Set1/AdditionRule.Rnw", "Set1/LoadedDie.Rnw", 
               "Set1/Options.Rnw", "Set1/BasicSets.Rnw", "Set1/WhichTyre1.Rnw", "Set1/WhichTyre2.Rnw")
 exams2pdf(chapter5, n = 1, tdir = "dummy5")
-
+exams2moodle(chapter5, n=5)
 
 chapter6 <- c("Cond1/BayesRuleProof.Rnw", "Cond1/BonzoFonzoGonzo.Rnw", "Cond1/IoA1.Rnw",
               "Cond1/IoA2.Rnw", "Cond1/ThreeUrnsRed.Rnw", "Cond1/ThreeUrnsGreen(Conditional).Rnw",
